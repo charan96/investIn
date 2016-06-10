e5885ba098d93d88df37c1bf0bb767f9f4185172
charan96 <charan.sudarsanam@gmail.com>
2016-06-10 16:31:08 -0500

    added forms; making some changes to views

--- stockSite/investIn/views.py
+++ stockSite/investIn/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import submitForm
 
 def index(request):
+	formClass = submitForm
+
+	if (request.method == 'POST'):
+		form = formClass(data=request.POST)
+
+		if form.is_valid():
+			ticker = request.POST.get('ticker','')
+
 	return render(request, 'investIn/index.html', {})
