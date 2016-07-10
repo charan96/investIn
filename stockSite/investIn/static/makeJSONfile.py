37cf99d8d06689e8a60fec2e4cc5c450adbf8ad3
charan96 <charan.sudarsanam@gmail.com>
2016-07-09 19:07:40 -0500

    made a few changes to the outlook of the webapp

---
+++ stockSite/investIn/static/makeJSONfile.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import json
+
+defOpts = "nj1l1c1p2poma2r"
+indexOpts = "snl1c1p2m"
+quandl_KEY = "d9c2StRA1P6VRoQFAdL2"
+strOfIndices = "^NYA,^XAX,^IXIC,^GSPC,^OEX,^SPSUPX,^VIX,^RUT,^FTSE,^FCHI,^GDAXI,^N225,^HSI,^STI,^SHCOMP"
+baseURL = "http://download.finance.yahoo.com/d/quotes.csv?s="
+optsURL = "&f="
+
+jsonDict = {"defOpts": defOpts, "indexOpts": indexOpts, "quandlKEY": quandl_KEY, "indicesStr": strOfIndices,
+		"baseURL": baseURL, "optsURL": optsURL}
+
+jsonFile = "coreData.json"
+
+with open(jsonFile, 'w') as outf:
+	json.dump(jsonDict, outf)
